04822a0cc3a17c658b7df3f14b6a7526b62d49ff
vinicius castro <castrovinicius09@gmail.com>
2025-10-18 11:48:03 -0300

    - swagger configuration

--- states_search/src/API/Program.cs
+++ states_search/src/API/Program.cs
@@ -1,5 +1,13 @@
+using API.Extensions;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSwaggerDependencies();
+
 WebApplication app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapGet("/", () => "Hello World!");
 
